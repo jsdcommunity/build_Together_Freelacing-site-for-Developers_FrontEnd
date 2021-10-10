d505706719fd35e4b5d6a8da737acfa73f4edbcb
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-10-10 13:02:19 +0530

    Basic home router setup with 'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
-import LogoCard from "./components/LogoCard/LogoCard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <LogoCard />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
