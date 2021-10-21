e2f7421e1b9ac5b1e6b7012cf21465d5559dab6f
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-10-21 14:37:18 +0530

    Removed unnecessary  code blocks from `src/redux/reducers/darkMode.js`

--- src/redux/reducers/darkMode.js
+++ src/redux/reducers/darkMode.js
@@ -2,10 +2,6 @@ const browserMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 const darkModeReducer = (state = browserMode, action) => {
   switch (action.type) {
-    case "DARK_MODE":
-      return true;
-    case "LIGHT_MODE":
-      return false;
     case "TOGGLE_MODE":
       return !state;
     default:
