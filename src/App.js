fcf6bccd3cc4fb751d7ec6ae05138c082e245402
shamilkotta <shamilkotta99@gmail.com>
2021-11-06 16:50:17 +0530

    explore page created and route added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useMemo } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Explore from "./pages/Explore";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
 import { CssBaseline } from "@mui/material";
@@ -31,6 +32,7 @@ function App() {
                   <Route exact path="/" component={Home} />
                   <Route path="/sign-up" component={SignUp} />
                   <Route path="/confirm-account" component={ConfirmAccount} />
+                  <Route path="/explore" component={Explore} />
                </Switch>
             </Router>
          </SnackbarProvider>
---
+++ src/pages/Explore.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import ExploreContent from "../components/ExploreContent/ExploreContent";
+
+const Explore = () => {
+  
+
+	return (
+		<>
+			<Header/>
+			<ExploreContent/>
+			<Footer/>
+		</>
+	)
+};
+
+export default Explore;
