40ab3e2e345dad7ce32d4b80177c0087c7f80344
Ajeesh cp <ajeeshcp5@gmail.com>
2021-11-13 20:23:04 +0530

    Added some contents in Hero section

--- src/components/HomePage/PageContents/HeroSection.jsx
+++ src/components/HomePage/PageContents/HeroSection.jsx
@@ -1,21 +1,27 @@
-import { Grid, Paper,Typography,Button } from '@mui/material'
+import { Grid, Paper,Typography,Button, Stack, Chip } from '@mui/material'
 import React from 'react'
 
 function HeroSection() {
     return (
-        <Paper elevation={3} sx={{mt:3,mx:2}}>
+        <Paper elevation={3} sx={{mt:4,mx:3}}>
             <Grid container spacing={2}>
                 <Grid item sx={{my:5}} xs={12} sm={6}>
-                    <Typography color="green" variant="h4" sx={{px:5}} gutterBottom component="div">
+                    <Typography sx={{px:{md:"100px",xs:"10px"},fontFamily:"sans-serif",fontWeight:"700",fontSize:{md:"30px",xs:"25px"}}} color="green" variant="h4"  component="div">
                         Find & Hire <br />
                         Expert Freelancers 
                     </Typography>
-                    <Typography variant="body1"  sx={{px:5}}>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+                    <Typography variant="subtitle1" sx={{fontSize:"16px",px:{md:"100px",xs:"10px"},my:1,fontStyle:"italic"}}>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
                 
                         Work with the best freelance talent from around the world           
                         flexible and cost-effective platform !
                     </Typography>
-                    <Button sx={{ml:5,mt:3}} variant="contained">Explore now </Button>
+                    <Stack sx={{px:{md:"100px",xs:"10px"}, my:1}} direction={{xs:"column",md:"row"}} spacing={1}>
+                        <Chip label="Android apps" color="primary" variant="outlined" />
+                        <Chip label="Web apps" color="success" variant="outlined" />
+                        <Chip label="Game Development" color="primary" variant="outlined" />
+                        <Chip label="Many more..." color="success" variant="outlined" />
+                    </Stack>
+                    <Button sx={{mx:{md:"100px",xs:"10px"}}} variant="contained">Explore now </Button>
                 </Grid>
                 <Grid display={{xs:"none",sm:"block"}} textAlign="center" item xs={12} sm={6}>
                    <img src="assets/hero-man.png" width="300px" alt="UpBit" />
