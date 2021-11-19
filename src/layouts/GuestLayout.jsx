ca12b90cabd87ecd9ae97f9be305608001507fec
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-11-19 21:56:14 +0530

    `react-router-dom` import fix

--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Redirect, Route, Switch } from "react-router";
+import { Redirect, Route, Switch } from "react-router-dom";
 import ConfirmAccount from "../pages/ConfirmAccount";
 import Explore from "../pages/Explore";
 import Home from "../pages/Home";
